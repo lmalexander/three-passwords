580d3de8630397054e2a46923d0714ea16268902
lmalexander <elizabeth.alexander@northwestern.edu>
2020-04-05 15:50:30 -0500

    working scripts.js, testing button functions. also updated UI

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const possibleChars = [
+    "!", "@", "#", "$", "%", "^", "&","*","()",")","_","-","=","{","}","[","]","~","1","2","3","4","5","6","7","8","9","0","q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","z","x","c","v","b","m","n",",",".","?","/","Q","W","E","R","T","Y","U","I","O","P","A","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"
+];
+
+const newPassword = [ ];
+
+function newPassword(possibleChars) {
+    const random = Math.floor(Math.random() * array.length);
+    const password = possibleChars[random];
+    console.log(password);
+    return password;
+}
+
+newPassword.push(password);
+
+$("genBtn").click(function() {
+    newPassword;
+})
